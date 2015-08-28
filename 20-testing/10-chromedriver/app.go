425540e4fb66b3a446eed7904c8d72160fb50855
Jacek Wysocki <jacek.wysocki@pearson.com>
2015-08-28 08:18:14 +0200

    more falgs

---
+++ 20-testing/10-chromedriver/app.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	wd "github.com/fedesog/webdriver"
+)
+
+const LOGIN_URL = "http://dev.ioki.com.pl/login"
+const USERNAME = "knowak"
+const PASSWORD = "ngml2"
+
+func main() {
+	nmel := NewNmel()
+	nmel.Goto(LOGIN_URL)
+	nmel.Login(USERNAME, PASSWORD)
+	nmel.Quit()
+}
+
+func NewNmel() *Nmel {
+	chromeDriver := wd.NewChromeDriver("chromedriver")
+	err := chromeDriver.Start()
+	if err != nil {
+		log.Println(err)
+	}
+
+	desired := wd.Capabilities{"Platform": "Linux"}
+	required := wd.Capabilities{}
+	session, err := chromeDriver.NewSession(desired, required)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return &Nmel{Driver: *chromeDriver, Session: *session}
+}
+
+type Nmel struct {
+	Driver  wd.ChromeDriver
+	Session wd.Session
+}
+
+func (this *Nmel) Login(user string, password string) (bool, error) {
+	element, err := this.Session.FindElement(wd.CSS_Selector, "#username")
+	if err != nil {
+		return false, err
+	}
+	element.SendKeys(user)
+
+	element, err = this.Session.FindElement(wd.CSS_Selector, "#password")
+	if err != nil {
+		return false, err
+	}
+	element.SendKeys(password)
+
+	log.Printf("Login'in as %s:%s\n", user, password)
+
+	element, err = this.Session.FindElement(wd.CSS_Selector, "#login_button")
+	if err != nil {
+		return false, err
+	}
+	element.Click()
+
+	return true, nil
+}
+
+func (this *Nmel) Goto(url string) (bool, error) {
+	log.Printf("Going to: %s\n", url)
+	this.Session.Url(url)
+
+	return true, nil
+}
+
+func (this *Nmel) Quit() {
+	log.Println("Byebye!")
+	time.Sleep(1 * time.Second)
+	this.Session.Delete()
+	this.Driver.Stop()
+	log.Println("Gracefully shuted down!")
+}
