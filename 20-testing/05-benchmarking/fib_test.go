134901503ec0d033d562ded4da04aa49aea01d28
Jacek Wysocki <jacek.wysocki@pearson.com>
2015-08-28 08:19:40 +0200

    benchmarking examples

--- 20-testing/05-benchmarking/fib_test.go
+++ 20-testing/05-benchmarking/fib_test.go
@@ -14,3 +14,4 @@ func BenchmarkFib3(b *testing.B)  { benchmarkFib(3, b) }
 func BenchmarkFib10(b *testing.B) { benchmarkFib(10, b) }
 func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
 func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
+func BenchmarkFib42(b *testing.B) { benchmarkFib(42, b) }
