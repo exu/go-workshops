2dd09f9b1930307dc14c52315d6a6e2d40483787
Jacek Wysocki <jacek.wysocki@pearson.com>
2015-09-29 05:18:02 +0200

    reorganized constannts example

--- 030-basics-constants/constants.go
+++ 030-basics-constants/constants.go
@@ -8,6 +8,18 @@ func main() {
 
 	// enumerated
 	const (
+		A float64 = iota
+		B
+		C
+		D
+		E
+		F
+	)
+
+	fmt.Println("Kojelno odlicz:", A, B, C, D)
+
+	// enumerated with arithmetics
+	const (
 		_          = iota // ignore
 		KB float64 = 1 << (10 * iota)
 		MB
@@ -22,17 +34,6 @@ func main() {
 	fmt.Println("Bytes", KB, MB, GB, TB)
 
 	const (
-		A float64 = iota
-		B
-		C
-		D
-		E
-		F
-	)
-
-	fmt.Println("Kojelno odlicz:", A, B, C, D)
-
-	const (
 		// Max integer value on 64 bit architecture.
 		maxInt = 9223372036854775807
 
@@ -40,6 +41,6 @@ func main() {
 		bigger = 9223372036854775808543522345
 
 		// Will NOT compile
-		// biggerInt int64 = 9223372036854775808543522345
+		// biggerint int64 = 9223372036854775808543522345
 	)
 }
