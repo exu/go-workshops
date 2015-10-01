d982905dc0805f8ca60aedeb8e4ac34e5a42ae38
Jacek Wysocki <jacek.wysocki@pearson.com>
2015-10-01 17:40:00 +0200

    appending data to slices

---
+++ 051-basics-slices/50-append-to-slices.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	cars := []string{"Ford Mustang", "Fiat 126p"}
+	fmt.Println(cars)
+	cars = append(cars, "BMV M5", "Tesla Roadster")
+	fmt.Println(cars)
+
+	motorbikes := []string{"Yamaha MT", "Hoda Goldwing"}
+
+	allVehicles := append(cars, motorbikes...)
+	fmt.Println(allVehicles)
+}
