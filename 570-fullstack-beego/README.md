3fdf43578fb030964bf2b6b0db5ec348aa026086
Jacek Wysocki <jacek.wysocki@pearson.com>
2015-11-09 09:22:45 +0100

    bee go draft

---
+++ 570-fullstack-beego/README.md
@@ -0,0 +1,12 @@
+## Beego
+
+Bee init script - inicjuje podstawową strukturę katalogów.
+hot compile.
+
+```
+go get github.com/astaxie/beego
+go get github.com/beego/bee
+bee new hello
+cd hello
+bee run hello
+```
---
+++ 570-fullstack-beego/app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
+func main() {
+	beego.Router("/", &MainController{})
+	beego.Run()
+}
