135149ebedb84593edd25e102ea022c7abc407b2
Jacek Wysocki <jacek.wysocki@pearson.com>
2018-03-03 10:29:19 +0100

    simplified lambda example

--- 700-lambda-simple/main.go
+++ 700-lambda-simple/main.go
@@ -1,37 +1,20 @@
+// main.go
 package main
 
 import (
-	"errors"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"log"
 )
 
-var (
-	// ErrNameNotProvided is thrown when a name is not provided
-	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
-)
+type Example struct {
+	Name        string
+	Description string
+}
 
-// Handler is your Lambda function handler
-// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
-// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
-
-	// If no name is provided in the HTTP request body, throw an error
-	if len(request.Body) < 1 {
-		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       "Hello " + request.Body,
-		StatusCode: 200,
-	}, nil
-
+func hello() (Example, error) {
+	return Example{"Hoł hoł Lambda!", "This is simple lamda example"}, nil
 }
 
 func main() {
-	lambda.Start(Handler)
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(hello)
 }
