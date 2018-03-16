25ae3063a899a6587290e790ebf56af7223e405d
Jacek Wysocki <jacek.wysocki@pearson.com>
2018-03-16 18:37:18 +0100

    translated regexp section

--- 180-stdlib-regexp/90-regexp.go
+++ 180-stdlib-regexp/90-regexp.go
@@ -6,53 +6,51 @@ import "regexp"
 
 func main() {
 
-	// testuje czy string zgadza się z narzuconym patternem
+	// tests if string match given pattern
 	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
 	fmt.Println(match)
 
-	// jezeli chcemy wykorzystywać częściej ten regexo kompilujemy go
+	// if we want use given expression more than once it'll be good to compile it
 	r, _ := regexp.Compile("p([a-z]+)ch")
 
-	// na skompilowanym obiekcie możemy wykonywać operacje
-	fmt.Println(r.MatchString("peach"))
+	// on compiled object we can call methods
+	fmt.Println("MatchString:", r.MatchString("peach"))
 
-	// znajduje match
-	fmt.Println(r.FindString("peach punch"))
+	// find matched string
+	fmt.Println("FindString:", r.FindString("peach punch"))
 
-	// Znajduje indeksy na podstawie patternu
-	fmt.Println(r.FindStringIndex("peach punch"))
+	// find string indexed based on given regexp
+	fmt.Println("FindStringIndex:", r.FindStringIndex("peach punch"))
 
-	// informacje na temat pierwszego napotkanego matcha i submatcha
-	fmt.Println(r.FindStringSubmatch("peach punch"))
+	// first match and submatch found
+	fmt.Println("FindStringSubmatch:", r.FindStringSubmatch("peach punch"))
 
-	// indeks submatch'a
-	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
+	// submatch index
+	fmt.Println("FindStringSubmatchIndex:", r.FindStringSubmatchIndex("peach punch"))
 
-	// wariant "All" odnosi się do wszystkich submatchy w regexp
-	fmt.Println(r.FindAllString("peach punch pinch", -1))
+	// `All` submatches
+	fmt.Println("FindAllString:", r.FindAllString("peach punch pinch", -1))
 
-	fmt.Println(r.FindAllStringSubmatchIndex(
-		"peach punch pinch", -1))
+	fmt.Println("FindAllStringSubmatchIndex:", r.FindAllStringSubmatchIndex("peach punch pinch", -1))
 
-	fmt.Println(r.FindAllStringSubmatch("peach punch", -1))
+	fmt.Println("FindAllStringSubmatch:", r.FindAllStringSubmatch("peach punch", -1))
 
-	// Limit ilości match'y
-	fmt.Println(r.FindAllString("peach punch pinch", 2))
+	// you can limit matches
+	fmt.Println("FindAllString:", r.FindAllString("peach punch pinch", 2))
 
-	// Wszystkie powyższe funkcje miały "String" w nazwie np: MatchString.
-	// Jeżeli mamy []byte to możemy ją przekazać do grupy
-	// fukcji bez String w nazwie
-	fmt.Println(r.Match([]byte("peach")))
+	// All methods above have "String" in name
+	// If we want to operate on `[]bytes` we can omit "String" in function name
+	fmt.Println("Match:", r.Match([]byte("peach")))
 
-	// MustCompile spanikuje jak nie uda się skompilować regexpa
+	// MustCompile will panic when expression will be invalid
 	r = regexp.MustCompile("p([a-z]+)ch")
-	fmt.Println(r)
+	fmt.Println("Compiled object:", r)
 
-	// Zamiana
-	fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
+	// replacing string
+	fmt.Println("ReplaceAllString:", r.ReplaceAllString("a peach", "<fruit>"))
 
-	// preg_replace_callback :)
+	// preg_replace_callback from PHP world :)
 	in := []byte("a peach")
 	out := r.ReplaceAllFunc(in, bytes.ToUpper)
-	fmt.Println(string(out))
+	fmt.Println("ReplaceAllFunc", string(out))
 }
