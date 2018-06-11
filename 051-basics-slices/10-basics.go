b0e86ea0cb74850fa8d59fc6b6f130bf564c152a
Jacek Wysocki <jacek.wysocki@pearson.com>
2018-06-11 11:39:19 +0200

    updated slices simple example

--- 051-basics-slices/10-basics.go
+++ 051-basics-slices/10-basics.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// create slice
+	// create slice dynamically
 	slice := []string{"one", "two", "three"}
-	fmt.Println(slice)
+	fmt.Printf("Dynamic: %+v\n", slice)
 	fmt.Println("Len: ", len(slice))
 	fmt.Println("Cap: ", cap(slice))
+
+	// create slice by `make`
+	allocated := make([]int, 5, 100)
+	fmt.Printf("Allocated: %+v\n", allocated)
+	fmt.Println("Len: ", len(allocated))
+	fmt.Println("Cap: ", cap(allocated))
+
 }
